5948c0f74c05689811b6019e7f30d4b102d106a1
Drake <drake,esdon@hotmail.com>
2019-12-12 16:16:35 -0500

    fixed nav bar

--- client/src/components/partials/main_nav.js
+++ client/src/components/partials/main_nav.js
@@ -71,15 +71,25 @@ function MainNav() {
           </li>
           <li className="nav-item dropdown">
             <a
-            className="nav-link dropdown-toggle"
-            href="/"
-            data-toggle="dropdown"
-            role="button"
-            aria-haspopup="true"
-            aria-expanded="false"
+              className="nav-link dropdown-toggle"
+              href="/"
+              data-toggle="dropdown"
+              role="button"
+              aria-haspopup="true"
+              aria-expanded="false"
             >
               Performances
             </a>
+
+            <div className="dropdown-menu">
+              <Link className="dropdown-item" to="/performances">
+                All Performances
+              </Link>
+              <Link className="dropdown-item" to="/performances/new">
+                New Performance
+              </Link>
+            </div>
+          </li>
           <li className="nav-item">
             <Link className="nav-link" to="/login">
               Login
